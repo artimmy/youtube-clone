cbc74d991518a4ca3d4a85a0b2ddda57309a160f
artimmy <arthurtimmy@hotmail.com>
2022-02-16 16:58:54 -0300

    adding a 'Video' class to be used on the 'Api' class

---
+++ lib/model/video.dart
@@ -0,0 +1,10 @@
+class Video
+{
+  String? id;
+  String? title;
+  String? description;
+  String? thumbnail;
+  String? channel;
+
+  Video({this.id, this.title, this.description, this.thumbnail, this.channel});
+}
