edd0f0d2a2a38092c30c4c7468569c19966b0106
artimmy <arthurtimmy@hotmail.com>
2022-02-16 22:16:42 -0300

    converting the map results into a list

--- lib/model/video.dart
+++ lib/model/video.dart
@@ -7,4 +7,16 @@ class Video
   String? channel;
 
   Video({this.id, this.title, this.description, this.thumbnail, this.channel});
+
+  factory Video.fromJson(Map<String, dynamic> receiving_a_map_JsonData_ToBeConvertedIntoAList)
+  {
+    var list = receiving_a_map_JsonData_ToBeConvertedIntoAList;
+    return Video(
+      id: list["id"]["videoId"],
+      title: list["snippet"]["title"],
+      thumbnail: list["snippet"]["thumbnails"]["high"]["url"],
+      channel: list["snippet"]["channelId"]
+    );
+
+  }
 }
