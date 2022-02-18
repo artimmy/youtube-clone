32663a45b606a04980ce9c50f283b71ac43ec054
artimmy <arthurtimmy@hotmail.com>
2022-02-17 21:32:26 -0300

    showing videos in a list (Listview). adding , Futurebuilder, _listVideos(), ListView.separated

--- lib/screens/app_home.dart
+++ lib/screens/app_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_clone/api.dart';
+import 'package:youtube_clone/model/video.dart';
 
 class AppHome extends StatefulWidget {
   const AppHome({ Key? key }) : super(key: key);
@@ -9,19 +10,71 @@ class AppHome extends StatefulWidget {
 }
 
 class _AppHomeState extends State<AppHome> {
+
+  _listVideos()
+  {
+    Api api = Api();
+    return api.search("");
+  }
+
   @override
   Widget build(BuildContext context) {
-    
-    Api api = Api();
-    api.search("");
 
-    return const Center(
-      child: Text(
-        "Home",
-        style: TextStyle(
-          fontSize: 25
-        ),
-      ),
+    return FutureBuilder<List<Video>?>(
+      future: _listVideos(),
+      builder: (context, snapshot)
+      {
+        switch (snapshot.connectionState) 
+        {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          case ConnectionState.active:
+          case ConnectionState.done:
+            if( snapshot.hasData )
+            {
+              return ListView.separated(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index)
+                {
+                  List<Video>? videos = snapshot.data;
+                  Video video = videos![ index ];
+
+                  return Column(
+                    children: <Widget>[
+                      Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(video.thumbnail.toString()),
+                          ),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(video.title.toString()),
+                        subtitle: Text(video.channel.toString()),
+                      )
+                    ],
+                  ); 
+                },
+                separatorBuilder: (context, index) => const Divider(
+                  height: 2,
+                  color: Colors.grey,
+                ), 
+                
+              );
+            }
+            break;
+        }
+        // Returning a Widget just so the FutureBuilder builder shut up.
+        // Also, taking advantage that futurebuilder asks for this Widget to handle errors.
+        return Center(
+          child: Text("Lists: Failed to load: ${snapshot.error.toString()}"),
+        );
+      },
     );
   }
 }
