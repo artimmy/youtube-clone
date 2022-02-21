826a4e7c413b7a629a10cc78dbcb60f204f4290b
artimmy <arthurtimmy@hotmail.com>
2022-02-21 16:37:12 -0300

    retrieving suggestions | list = [...].where(...).toList() | list with suggestions

--- lib/custom_search_delegate.dart
+++ lib/custom_search_delegate.dart
@@ -32,7 +32,7 @@ class CustomSearchDelegate extends SearchDelegate<String>
 
   @override
   Widget buildResults(BuildContext context) {
-    // when the user presses search this class returns the query (which is the search field text)
+    // When the user presses search this class returns the query (which is the search field text)
     close(context, query);
     return Container();
   }
@@ -40,7 +40,41 @@ class CustomSearchDelegate extends SearchDelegate<String>
   @override
   Widget buildSuggestions(BuildContext context) {
     // Can suggest things for the user based on what their're typing.  
-    return Container();
+    List<String> list = [];
+    // If the user didnt type anything yet then else...
+    if ( query.isNotEmpty )
+    {
+      // Creating a manual list of possible words. Could be done using a autocomplete api.
+      list = [
+        "Android", "Automobiles", "Automation", "IOS", "Games", "Blockchain", "Bob Ross", "Cars", "Detailing", "Flying", "Gangstar Paradise",
+        // Filter this list selecting a list item 'word' where this word starts with what the user is typing 'query'.
+      ].where(
+        (word) => word.toLowerCase().startsWith( query.toLowerCase() )
+        // Converting it back to a list / returning a list with the words tha match.
+      ).toList();
+
+      // Creating a ListView with 'list' items.
+      return ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index)
+        {
+          return ListTile(
+            title: Text( list[index] ),
+            // When the user clicks the list item i.e. selecting a suggestion, whis will close the search and
+            // return the list item clicked as the 'query'
+            onTap: () => close(context, list[index]),
+          );
+        }
+      );
+    }
+    else 
+    {
+      // This will apear only when 'query' is empty
+      return const Center(
+        child: Text("No Results"),
+      );
+    }
+    // return Container();
   }
 
 }
